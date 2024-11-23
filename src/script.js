f9f47e443e148f93089947008c08ce8031d49ab0
estephanysvg <estephanysvg@gmail.com>
2024-11-23 15:23:04 +0100

    Probando iluminación en tres objetos

--- src/script.js
+++ src/script.js
@@ -3,16 +3,30 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
  * Base
- */
+*/
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
-
 // Scene
 const scene = new THREE.Scene()
 
+
+// Lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 30)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+
+scene.add(pointLight)
+
+
 /**
  * Sizes
- */
+*/
+
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -22,11 +36,11 @@ window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
-
+    
     // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
-
+    
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -34,19 +48,65 @@ window.addEventListener('resize', () => {
 
 /**
  * Camera
- */
+*/
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+scene.add(camera)
 camera.position.x = 1
 camera.position.y = 1
 camera.position.z = 2
-scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-const material = new THREE.MeshBasicMaterial()
+/**
+ * Textures
+*/
+
+const textureLoader = new THREE.TextureLoader()
+
+const doorColorTexture = textureLoader.load('./textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('./textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('./textures/door/ambientOcclusion.jpg')
+const doorMetalnessTexture = textureLoader.load('./textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('./textures/door/roughness.jpg')
+const matcapTexture = textureLoader.load('./textures/matcaps/8.png')
+const gradientTexture = textureLoader.load('./textures/gradient/3.jpg')
+
+doorColorTexture.colorSpace = THREE.SRGBColorSpace
+matcapTexture.colorSpace = THREE.SRGBColorSpace
+
+// Objetos
+
+// MeshBasicMaterial
+
+// const material = new THREE.MeshBasicMaterial({ map: doorColorTexture})
+// const material = new THREE.MeshBasicMaterial()
+// material.map = doorColorTexture
+// material.color = new THREE.Color('red')
+// material.wireframe = true
+// material.transparent = true
+// material.opacity = 0.2
+// material.alphaMap = doorAlphaTexture
+// material.side = THREE.DoubleSide
+
+// MeshNormalMaterial
+
+// const material = new THREE.MeshNormalMaterial()
+// material.wireframe = true
+// material.flatShading = true
+
+// MeshMatCapMaterial
+// const material = new THREE.MeshMatcapMaterial()
+// material.matcap = matcapTexture
+
+//MeshDeptMaterial
+// const material = new THREE.MeshDepthMaterial()
+
+// MeshLambertMaterial
+const material = new THREE.MeshLambertMaterial()
+
 
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 16, 16), material
@@ -87,7 +147,7 @@ const tick = () => {
     plane.rotation.y = 0.1 * elapsedTime
     torus.rotation.y = 0.1 * elapsedTime
 
-    
+
     sphere.rotation.x = -0.15 * elapsedTime
     plane.rotation.x = -0.15 * elapsedTime
     torus.rotation.x = -0.15 * elapsedTime
